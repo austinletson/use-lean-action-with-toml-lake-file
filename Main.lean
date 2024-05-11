0dc65809a84954d630a1667950b3a4976cdd8990
austinletson <waustinletson@gmail.com>
2024-05-11 08:25:31 -0400

    feat: lake init and ci.yml

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import «UseLeanActionWithTomlLakeFile»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ UseLeanActionWithTomlLakeFile.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `UseLeanActionWithTomlLakeFile` library.
+-- Import modules here that should be built as part of the library.
+import «UseLeanActionWithTomlLakeFile».Basic
